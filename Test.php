dafbeb330aff8b5c86c3e473124ecfa9bd8a8869
ami0316 <ncxsh229@yahoo.co.jp>
2024-07-11 03:45:15 +0000

    php2-2

--- Test.php
+++ Test.php
@@ -18,3 +18,22 @@ if($n == 1){
     echo "嫌い";
 }
 ?>
+
+<?php
+$place = rand(1,5); // 順位を1〜5の範囲でランダムに作られた数字を$placeに代入
+echo "placeの中身:".$place."\n";
+if($place == 1){
+    echo "金賞";
+    // 1位だったときの処理
+}elseif($place == 2){
+    echo "銀賞";
+}elseif($place == 3){
+    echo "銅賞";
+ // code...
+    
+}else{
+    // それ以外だったときの処理
+    echo $place."位";
+}
+
+?>
