a1e8779974ecadd0b534e53fc8d22f39bf022f3c
ami0316 <ncxsh229@yahoo.co.jp>
2024-07-14 10:01:47 +0000

    php2-5

--- Test.php
+++ Test.php
@@ -73,3 +73,14 @@ if($omikuji >= 30){
     echo "omikujiの中身は".$omikuji."なので大凶";
 }
 ?>
+
+<?php
+
+$dice = rand(1,6);
+echo "サイコロは".$dice."\n";
+if ($dice >= 4 && $dice <= 6) {
+    echo "スライムの攻撃をかわした";
+}else{
+    echo "スライムから10のダメージを受けた";
+}    
+?>
