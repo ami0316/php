4889ee6a169a74cece80a2950ac893f0dfa3cc20
ami0316 <ncxsh229@yahoo.co.jp>
2024-07-16 03:46:52 +0000

    update

--- Test.php
+++ Test.php
@@ -87,11 +87,3 @@ echo "西暦".$year."年は";
 $hesei = $year - 1988;
 echo "平成".$hesei."年です";
 ?>
-
-<?php
-$year = rand(1926,1988);
-echo "西暦".$year."年は";
-$syowa = 0000; // 昭和年の計算式を書く
-echo "昭和".$syowa."年です";
-
-?>
