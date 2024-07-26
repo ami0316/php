68fc373dff000ef7d102517dff982a6e6e2700a9
ami0316 <ncxsh229@yahoo.co.jp>
2024-07-26 00:58:48 +0000

    ph10-2

--- Test.php
+++ Test.php
@@ -147,3 +147,15 @@ class Box {
     }
 }
 ?>
+
+<?php
+echo "start\n";
+try {
+    $date = new DateTime("199x-01-01");
+    echo $date->format('Y/m/d'). "\n";
+} catch (Exception $e) {
+    echo $e->getMessage() . "\n";
+} finally {
+    echo "end\n";
+}    
+?>
