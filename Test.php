2277ddd73072ab4e319a5df9a9efa5f90c616764
ami0316 <ncxsh229@yahoo.co.jp>
2024-07-28 02:12:07 +0000

    php9-2practice

--- Test.php
+++ Test.php
@@ -168,3 +168,26 @@ $date = new DateTime("199x-01-01");
 echo $date->format('Y/m/d') . "\n";
 echo "end\n";
 ?>
+
+<?php
+// クラスにメソッドを定義しよう
+class Greeting {
+    public $msg;
+    public $target;
+
+    public function __construct() {
+        $this->msg = "hello";
+        $this->target = "paiza";
+    }
+    public function sayHello() {
+        echo $this->msg." ".$this->target;
+    }
+}
+
+class Hello extends Greeting {
+}
+
+$hello = new Hello();
+// 続けて、sayHelloメソッドを呼び出す
+
+?>
