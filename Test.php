f6cabd21088f2eb06cfd1ece36062bf17e9af258
ami0316 <ncxsh229@yahoo.co.jp>
2024-08-06 07:02:11 +0000

    php6-1practice1

--- Test.php
+++ Test.php
@@ -289,3 +289,20 @@ function sayHello(){
     echo "hello paiza";
 }
  ?>
+ 
+ <?php
+// 2次元配列を作成してみよう
+$item1 = ["木の棒", "こん棒"];
+$item2 = ["おにぎり", "おにぎり"];
+$item3 = ["毒消し", "薬草"];
+
+// item1 ~ 3を、$basket配列に代入してください。
+$basket = [];
+
+echo $basket[0][0]."\n";
+echo $basket[0][1]."\n";
+echo $basket[1][0]."\n";
+echo $basket[1][1]."\n";
+echo $basket[2][0]."\n";
+echo $basket[2][1]."\n";
+?>
