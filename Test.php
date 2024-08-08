feb35b1a99c318fd3d7583bd9048f2108add4520
ami0316 <ncxsh229@yahoo.co.jp>
2024-08-08 04:55:27 +0000

    php6-1practice2

--- Test.php
+++ Test.php
@@ -306,3 +306,16 @@ echo $basket[1][1]."\n";
 echo $basket[2][0]."\n";
 echo $basket[2][1]."\n";
 ?>
+
+<?php
+// 配列の中身を出力してみよう
+$members = [["勇者", "忍者"], ["武士", "戦士"], ["僧侶", "魔法使い"]];
+
+// この下で、$members配列の全ての要素を出力してみよう
+echo($members[0][0])."\n";
+echo($members[0][0])."\n";
+echo($members[0][0])."\n";
+echo($members[0][0])."\n";
+echo($members[0][0])."\n";
+echo($members[0][0])."\n";
+?>
