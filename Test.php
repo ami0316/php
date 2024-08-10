3adf35ebd8e6ea8d144141052927a68d1d56f379
ami0316 <ncxsh229@yahoo.co.jp>
2024-08-10 02:22:50 +0000

    php５-２practice2

--- Test.php
+++ Test.php
@@ -329,3 +329,21 @@ $item =
 print_r($item);
 
 ?>
+
+<?php
+// 標準入力を３行取得
+for($i=0 ; $i<3 ; $i++){
+	$input = trim(fgets(STDIN));
+	// 入力値を/で分割
+	$key_value =explode("/", $input);
+	// 手前の要素をkeyとして、後の要素をvalueとして連想配列に代入
+	$key = $key_value[0];
+	$value = $key_value[1];
+	$team[$key] = $value;
+
+}
+// ここから下に記述　
+// $team のkeyが戦士のvalueを出力
+
+
+?>
