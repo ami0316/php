0443ed75215c75681a56490633c8649e88647084
ami0316 <ncxsh229@yahoo.co.jp>
2024-08-13 02:31:56 +0000

    php５-２practice5

--- Test.php
+++ Test.php
@@ -386,3 +386,23 @@ for($i=0 ; $i<3 ; $i++){
 print_r($team);
 
 ?>
+
+<?php
+// 標準入力を３行取得
+for($i=0 ; $i<3 ; $i++){
+	$input = trim(fgets(STDIN));
+	// 入力値を/で分割
+	$key_value =explode("/", $input);
+	// 手前の要素をkeyとして、後の要素をvalueとして連想配列に代入
+	$key = $key_value[0];
+	$value = $key_value[1];
+	$team[$key] = $value;
+
+}
+// ここから下に記述　
+// $team のkeyが「スライム」の要素を削除して
+// print_rで $team の内容を出力してみよう。
+
+print_r($team);
+
+?>
